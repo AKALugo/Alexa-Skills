1a04fe6caf353a167e6b5c9f578db5a8b3d65242
Alejandro Lugo Fumero <64960467+AKALugo@users.noreply.github.com>
2022-07-31 16:48:34 +0100

    En qué ciudad ocurrió Skill

---
+++ En qué ciudad ocurrió Skill/lambda/question-list.js
@@ -0,0 +1,36 @@
+module.exports = {
+    '0' : {
+      'id' : '0',
+      'question' : 'hasta la década de los 20, el 1 de mayo era el día de la mudanza',
+      'city' : 'nueva york',
+      'answer' : 'los que tenían pensado mudarse de casa esperaban a esa fecha del año, así que todo el mundo hacía la mudanza a la vez. \
+                  Esa tradición, que se remontaba a la época colonial, comenzó a desaparecer a media que crecía la ciudad, ya que el \
+                  día de la mudanza acabó siendo demasiado caótico, ¡las calles se quedaban bloqueadas con tanta carreta tirada por caballos! \
+                  Afortunadamente, ahora los neoyorquinos pueden mudarse (y se mudan) cualquier día del año',
+      'clue' : 'Ciudad de los Estados Unidos de América'
+    },
+    '1' : {
+      'id' : '1',
+      'question' : 'fue Bombardeada en la guerra civil española',
+      'city' : 'belchite', 
+      'answer' : 'La batalla de Belchite fue una batalla de la guerra civil española que tuvo lugar en Belchite (Zaragoza) entre el 24 de agosto \
+                  y el 6 de septiembre de 1937 en el marco de la ofensiva del Ejército Popular sobre Zaragoza.',
+      'clue' : 'Municipio de la provincia de Zaragoza en Aragón'
+    },
+    '2' : {
+      'id' : '2',
+      'question' : 'le declaró la guerra a Francia ella sola',
+      'city' : 'líjar',
+      'answer' : 'El pueblo de Líjar, que se compone únicamente de trescientos vecinos y seiscientos hombres útiles, está dispuesto a declararle guerra\
+                  a toda la Francia. Era 14 de octubre de 1883',
+      'clue' : 'Municipio español de la provincia de Almería, Andalucía.'
+    },
+    '3' : {
+      'id' : '3',
+      'question' : 'promulgó la Constitución española de 1812',
+      'city' : 'cádiz',
+      'answer' : 'La Constitución española de 1812 o Constitución de Cádiz, conocida popularmente como la Pepa, fue promulgada por las Cortes Generales \
+                  españolas reunidas extraordinariamente en Cádiz el 19 de marzo de 1812',
+      'clue' : 'Antigua ciudad portuaria en el suroeste de España, construida en una franja de tierra rodeada de mar en la región de Andalucía. '
+    }
+};
